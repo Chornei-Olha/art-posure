47b1badc39d583c70b6b7100c9ee6b61f526b8bb
Olha Chornei <Chornei8053@gmail.com>
2026-05-22 02:08:57 +0300

    added new commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://art-posture.com/sitemap.xml',
+    sitemap: 'https://art-posure.com/sitemap.xml',
   };
 }
